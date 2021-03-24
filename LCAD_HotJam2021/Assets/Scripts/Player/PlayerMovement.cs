319dc8237c492fb9fdd7ee9f798f3235abad8d46
Aleroms <santiagomorales333@yahoo.com>
2021-03-23 19:21:40 -0700

    basic audio for player movement

--- LCAD_HotJam2021/Assets/Scripts/Player/PlayerMovement.cs
+++ LCAD_HotJam2021/Assets/Scripts/Player/PlayerMovement.cs
@@ -13,10 +13,16 @@ public class PlayerMovement : MonoBehaviour
 
     private Animator _animator;
 
+    private AudioSource _audioSource;
+
+    private bool _isMoving = false;
+
     void Start()
     {
         _rb = GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>();
         _animator = GameObject.FindGameObjectWithTag("Player").GetComponent<Animator>();
+        _audioSource = GetComponent<AudioSource>();
+        if (_audioSource == null) Debug.LogError("audio src null");
     }
 
 	private void Update()
@@ -29,6 +35,21 @@ public class PlayerMovement : MonoBehaviour
         _animator.SetFloat("Horizontal", movement.x);
         _animator.SetFloat("Vertical", movement.y);
         _animator.SetFloat("Speed", movement.sqrMagnitude);
+
+        //audio
+        if (movement.x != 0 || movement.y != 0)
+            _isMoving = true;
+        else
+            _isMoving = false;
+
+        if (_isMoving)
+        {
+            if (!_audioSource.isPlaying)
+                _audioSource.Play();
+        }
+        else
+            _audioSource.Stop();
+            
 	}
 	// Update is called once per frame
 	private void FixedUpdate()
